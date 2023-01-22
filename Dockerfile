2cef694a67a76e37b4d42a80346ba0d44fdba7f9
arijitbhowmick86 <44547301+arijitbhowmick86@users.noreply.github.com>
2023-01-22 23:20:55 +0530

    Docker changes

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+from python:3.9-slim-buster
+
+WORKDIR /flash-docker
+
+RUN python3 -m pip install --upgrade pip
+
+COPY requirements.txt requirements.txt
+
+RUN pip3 install -r requirements.txt
+
+COPY . .
+
+CMD ["python3", "-m", "flask", "run", "--host=0.0.0.0"]
+
--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/ping', methods=['GET'])
-def test_connection():
-    return 'Connected !'
+@app.route('/ping/<name>', methods=['GET'])
+def test_connection(name):
+    return f'Welcome Mr. {name} !'
+
+
+# docker build -t flask_app .
+# docker run -d -p 8000:5000 flask_app
